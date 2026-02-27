41de1a3652758bcafece7b94c7b24f555d62e9a9
melanielinaresdelacruz-sketch <melanielinaresdelacruz@gmail.com>
2026-02-27 16:28:58 -0600

    Add files via upload
    cambio de url

--- supabase.js
+++ supabase.js
@@ -3,7 +3,7 @@
  VERSION ESTABLE VERCEL
 **************************/
 
-const SUPABASE_URL = "https://mbllpmydlewmdzcnxpcq.supabase.co";
+const SUPABASE_URL = "https://usugqvmyfrxvevnizody.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVzdWdxdm15ZnJ4dmV2bml6b2R5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyMjc5NDYsImV4cCI6MjA4NzgwMzk0Nn0.XZDxzEPnYsbeQ_BlM6DzBSMy39_VPxrxwGHoMSAy-Eg";
 
 /* Verificar que el CDN esté cargado */
