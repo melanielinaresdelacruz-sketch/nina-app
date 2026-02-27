7cffdfc81b7f9c27ff1d64542639013f608e1fe0
melanielinaresdelacruz-sketch <melanielinaresdelacruz@gmail.com>
2026-02-27 14:45:20 -0600

    Add files via upload
    cambio a anon public key

--- supabase.js
+++ supabase.js
@@ -4,7 +4,7 @@
 **************************/
 
 const SUPABASE_URL = "https://mbllpmydlewmdzcnxpcq.supabase.co";
-const SUPABASE_KEY = "TU_PUBLIC_ANON_KEY_AQUI";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ibGxwbXlkbGV3bWR6Y254cGNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzExMzk3MjcsImV4cCI6MjA4NjcxNTcyN30.9_mbG9jZDmwT-AKa4sDS9oVwdVcbIwO95f7yKW-yBRk";
 
 /* Verificar que el CDN esté cargado */
 if (typeof window.supabase === "undefined") {
